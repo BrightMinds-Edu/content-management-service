37145b33380f02eee3f84b2da76956f815c4b7d6
Leshmith <leshmith1212@gmail.com>
2025-06-13 10:47:44 +0530

    Setup deployment in AWS as a lambda function and connected with hosted frontend.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('api/data')
+  getData(): { message: string } {
+    return { message: 'Hello from the backend!' };
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS for the frontend's origin and allow credentials
+  app.enableCors({
+    origin: 'http://brightminds-deployments.s3-website.eu-north-1.amazonaws.com', // Allow requests from this origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
+    credentials: true, // Allow credentials
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
